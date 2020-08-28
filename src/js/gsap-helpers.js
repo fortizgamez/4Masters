b03bc39ea62c4c11d254578025a73885cd000fef
fortizgamez <fortizgamez@gmail.com>
2020-08-28 17:53:06 +0200

    Add distribute

--- src/js/gsap-helpers.js
+++
@@ -1,9 +0,0 @@
-/*
-Function that "start" a timeline object and "reverse" it
-when it an event in a specified element is triggered.
-*/
-export const startAndReverse = (elem, event, tl) => elem
-    .addEventListener(
-        event,
-        () => tl.reversed() ? tl.play() : tl.reverse()
-    );
