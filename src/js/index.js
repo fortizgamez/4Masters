d387e720f43a8573520e3921a4cc88392e2150c9
fortizgamez <fortizgamez@gmail.com>
2020-09-02 23:44:19 +0200

    Performance Issues

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,18 @@ import './artists/velazquez_scene';
 import './artists/goya_scene';
 import './artists/picasso_scene';
 import './artists/dali_scene';
+
+const loadTime = () => {
+  // Get current time.
+  const now = new Date().getTime();
+  // Calculate page load time.
+  const pageLoadTime = now - performance.timing.navigationStart;
+  // Write the load time to the F12 console.
+  if (window.console) console.log(`Page Load Time: ${pageLoadTime}`);
+};
+
+window.addEventListener('load', loadTime, false);
+
+const perfEntries = performance.getEntriesByType('navigation');
+console.log(`TimeStamp to Load: ${perfEntries.domContentLoadedEventEnd - perfEntries.domContentLoadedEventStart}`);
+console.log(perfEntries);
