d0a58bd1b9823c690529709ae6fd712d5acca4c0
fortizgamez <fortizgamez@gmail.com>
2020-09-03 21:33:08 +0200

    Add New Bundler

--- src/js/artists/velazquez_scene.js
+++ src/js/artists/velazquez_scene.js
@@ -21,69 +21,117 @@ const velazquezImage5 = document.querySelectorAll('#velazquez-image-5');
 const velazquezImage6 = document.querySelectorAll('#velazquez-image-6');
 const perspective = 1000;
 
-gsap.timeline({
-  scrollTrigger: {
-    trigger: section,
-    start: 'top top',
-    end: '+=10000',
-    pin: true,
-    anticipatePin: 2,
-    scrub: true,
-  },
-})
-  .addLabel('welcome_effect')
-  .to(welcomeSection, 1, { css: { z: perspective } })
-  .fromTo(imgFrame,
-    { filter: 'grayscale(100%) brightness(1.5)' },
-    { duration: 1, filter: 'grayscale(0) brightness(1)' }, 'welcome_effect-=0.3')
-  .fromTo(sectionInfo, 1, { opacity: 0 }, { opacity: 1 })
-  .addLabel('show_info_1')
-  .addPause(3)
-  .to(velazquezInfo1, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo2, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addPause(3)
-  .to(velazquezInfo2, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo3, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addPause(3)
-  .to(velazquezInfo3, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo4, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addLabel('show_info_4')
-  .addPause(3)
-  .to(velazquezInfo4, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo5, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addPause(3)
-  .to(velazquezInfo5, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo6, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addPause(3)
-  .to(velazquezInfo6, 1, { opacity: 0, display: 'none' })
-  .fromTo(velazquezInfo7, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
-  .addPause(3)
-  .from(velazquezImage2, {
-    duration: 12,
-    y: 15,
-    z: '-600px',
-  }, 'show_info_1')
-  .from(velazquezImage3, {
-    duration: 12,
-    y: 15,
-    z: '-600px',
-  }, 'show_info_1')
-  .from(velazquezImage4, {
-    duration: 12,
-    y: -30,
-    z: '-500px',
-  }, 'show_info_1')
-  .from(velazquezImage5, {
-    duration: 12,
-    x: 120,
-    y: -100,
-    filter: 'blur(6px)',
-    z: '-450px',
-  }, 'show_info_1')
-  .from(velazquezImage6, {
-    duration: 12,
-    x: 65,
-    y: -40,
-    z: '-450px',
-    filter: 'blur(6px)',
-  }, 'show_info_1');
+const largeScreenTimeline = () => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: '+=10000',
+      pin: true,
+      anticipatePin: 2,
+      scrub: 0.5,
+    },
+  })
+    .addLabel('welcome_effect')
+    .to(welcomeSection, 1, { css: { z: perspective } })
+    .fromTo(imgFrame,
+      { filter: 'grayscale(100%) brightness(1.5)' },
+      { duration: 1, filter: 'grayscale(0) brightness(1)' }, 'welcome_effect-=0.3')
+    .fromTo(sectionInfo, 1, { opacity: 0 }, { opacity: 1 })
+    .addLabel('show_info_1')
+    .addPause(3)
+    .to(velazquezInfo1, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo2, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo2, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo3, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo3, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo4, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addLabel('show_info_4')
+    .addPause(3)
+    .to(velazquezInfo4, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo5, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo5, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo6, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo6, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo7, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .from(velazquezImage2, {
+      duration: 12,
+      y: 15,
+      z: '-950px',
+    }, 'show_info_1')
+    .from(velazquezImage3, {
+      duration: 12,
+      y: 15,
+      z: '-950px',
+    }, 'show_info_1')
+    .from(velazquezImage4, {
+      duration: 12,
+      y: -30,
+      z: '-900px',
+    }, 'show_info_1')
+    .from(velazquezImage5, {
+      duration: 12,
+      x: 120,
+      y: -100,
+      filter: 'blur(6px)',
+      z: '-850px',
+    }, 'show_info_1')
+    .from(velazquezImage6, {
+      duration: 12,
+      x: 65,
+      y: -40,
+      z: '-850px',
+      filter: 'blur(6px)',
+    }, 'show_info_1');
+};
+
+const allScreenTimeline = () => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      start: 'top top',
+      end: '+=10000',
+      pin: true,
+      anticipatePin: 2,
+      scrub: 0.5,
+    },
+  })
+    .addLabel('welcome_effect')
+    .to(welcomeSection, 1, { css: { z: perspective } })
+    .fromTo(imgFrame,
+      { filter: 'grayscale(100%) brightness(1.5)' },
+      { duration: 1, filter: 'grayscale(0) brightness(1)' }, 'welcome_effect-=0.3')
+    .fromTo(sectionInfo, 1, { opacity: 0 }, { opacity: 1 })
+    .addLabel('show_info_1')
+    .addPause(3)
+    .to(velazquezInfo1, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo2, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo2, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo3, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo3, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo4, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addLabel('show_info_4')
+    .addPause(3)
+    .to(velazquezInfo4, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo5, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo5, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo6, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3)
+    .to(velazquezInfo6, 1, { opacity: 0, display: 'none' })
+    .fromTo(velazquezInfo7, 1, { opacity: 0 }, { opacity: 1, display: 'block' })
+    .addPause(3);
+};
+
+ScrollTrigger.matchMedia({
+  '(min-width: 1024px)': largeScreenTimeline,
+  // eslint-disable-next-line quote-props
+  'all': allScreenTimeline,
+});
